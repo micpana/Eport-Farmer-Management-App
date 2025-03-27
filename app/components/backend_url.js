c0fab99e29a26c887acbc2675911e4d90718f283
micpana <michaelmudimbu@gmail.com>
2025-03-27 12:18:08 +0200

    added init.d for backend server startups + added new backend url to the app + added new apk with the new url

--- app/components/backend_url.js
+++ app/components/backend_url.js
@@ -1,2 +1,3 @@
-export const Backend_Url = 'http://192.168.1.2:5000/' ///LOCAL DEVELOPMENT SERVER
-// export const Backend_Url = '' ///CLOUD SERVER (LIVE)
+// export const Backend_Url = 'http://192.168.1.2:5000/' ///LOCAL DEVELOPMENT SERVER
+export const Backend_Url = 'https://farm5.eport.dev/' ///CLOUD SERVER (LIVE)
+// export const Backend_Url = 'https://sandbox.farm5.eport.dev/' ///CLOUD SERVER (LIVE 2)
